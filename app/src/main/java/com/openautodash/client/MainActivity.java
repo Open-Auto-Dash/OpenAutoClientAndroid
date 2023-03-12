6e2fa1ef7db42be7aa50d099258553b4fdb59a5a
Edgar Wideman <edywide@gmail.com>
2023-03-11 20:22:47 -0500

    location receiver
    Signed-off-by: Edgar Wideman <edywide@gmail.com>

--- app/src/main/java/com/openautodash/client/MainActivity.java
+++ app/src/main/java/com/openautodash/client/MainActivity.java
@@ -2,11 +2,17 @@ package com.openautodash.client;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.openautodash.client.ui.main.MainFragment;
 
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +22,20 @@ public class MainActivity extends AppCompatActivity {
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.container, MainFragment.newInstance())
                     .commitNow();
+        }
+
+        Uri locationUri = getIntent().getData();
+
+        if (locationUri != null) {
+            String location = locationUri.toString();
+            Log.d(TAG, "onCreate: Location String :" + location);
+            //decode location
+            String[] locationArray = location.split("[^,]*(?=\\\\?)");
+
+            Log.d(TAG, "onCreate: Striped string is: " + Arrays.toString(locationArray));
+
+
+            Toast.makeText(this, location, Toast.LENGTH_LONG).show();
         }
     }
 }
